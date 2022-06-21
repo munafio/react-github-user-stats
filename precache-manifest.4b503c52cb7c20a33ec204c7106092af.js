95a6bc5a28f701a3669eedf561b15474c47f57d2
munafio <munafaqeelmahdi@gmail.com>
2022-06-21 04:41:44 +0300

    Updates

---
+++ precache-manifest.4b503c52cb7c20a33ec204c7106092af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a475f5d2ccc3fe030829193b33bc14",
+    "url": "/react-github-user-stats/index.html"
+  },
+  {
+    "revision": "57bc3d8dc12b1be74b88",
+    "url": "/react-github-user-stats/static/css/main.314562a5.chunk.css"
+  },
+  {
+    "revision": "60582fd92576dbf9e526",
+    "url": "/react-github-user-stats/static/js/2.af7cb70f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-github-user-stats/static/js/2.af7cb70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bc3d8dc12b1be74b88",
+    "url": "/react-github-user-stats/static/js/main.f951f580.chunk.js"
+  },
+  {
+    "revision": "59c92c5b5fce2b1c8628",
+    "url": "/react-github-user-stats/static/js/runtime-main.6239e689.js"
+  }
+]);
